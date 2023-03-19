fbb217ca8422877b80865ea1eaa9913c857a7f71
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-03-19 11:33:39 +0200

    1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+export const Profile = ({ user: { username, tag, location, avatar, stats } }) => {
+    return <div>
+<Profile
+  username={username}
+  tag={tag}
+  location={location}
+  avatar={avatar}
+  stats={stats}
+/>
+    </div>
+};
